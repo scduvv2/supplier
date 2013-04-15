4146b70ca6444aa71b39b9dbbaf4a23034d02153
sarat <sarat.duvvuri@gmail.com>
2013-04-14 20:08:29 -0400

    Initial Commit

---
+++ src/main/java/com/smartshop/web/config/RootConfig.java
@@ -0,0 +1,18 @@
+package com.smartshop.web.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@ComponentScan(basePackages = { "com.smartshop.web" })
+public class RootConfig {
+	
+	@Bean
+	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+		ppc.setLocation(new ClassPathResource("/persistence.properties"));
+		return ppc;
+	}
+	
+}
